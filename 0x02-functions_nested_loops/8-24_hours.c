09ec575ee00967083a021667f431c5f24f73848f
effiecancode <faitheffie25@gmail.com>
2023-02-22 12:03:06 -0800

    The 24_hours commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints time
+ * Description: prints time including hours and minutes
+ * Return: sum of two  numbers
+ */
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
+
