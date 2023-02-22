9274525607bee1b25dd1b8d0dc7fc6c2433ce239
effiecancode <faitheffie25@gmail.com>
2023-02-22 12:09:36 -0800

    The 10-add commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * Description: Adds two integers and returns the result
+ * @a: The first number to be added
+ * @b: The second number to be added
+ * Return: the sum of two numbers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
