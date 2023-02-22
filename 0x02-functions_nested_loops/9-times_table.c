f61df6f22cdaab3ee2ac074af14b521abe2318d9
effiecancode <faitheffie25@gmail.com>
2023-02-22 12:06:46 -0800

    The times_table commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - prints time table
+ * Description: prints time tables from 0 - 9
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int a, b, res;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			res = (a * b);
+			if (b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (res >= 10)
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+			else if (res < 10 && b != 0)
+			{
+				_putchar(' ');
+				_putchar((res % 10) + '0');
+			}
+			else
+				_putchar((res % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
