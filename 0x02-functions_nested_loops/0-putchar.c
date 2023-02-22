db8b72178a193b68840a64405105def86857613b
effiecancode <faitheffie25@gmail.com>
2023-02-21 22:22:48 -0800

    The putchar ccommit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
-/
+/**
  * main - Prints Holberton as a message.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-  int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-  int count, sz;
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int count, sz;
 
-  sz = sizeof(str) / sizeof(int);
-  for (count = 0; count < sz; count++)
-  {
-    _putchar(str[count]);
-  }
-  _putchar('\n');
-  return (0);
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
 }
