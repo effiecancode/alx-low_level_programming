7ff629a54524bc00a884e4495f6774957db24a27
effiecancode <faitheffie25@gmail.com>
2023-02-22 09:28:00 -0800

    The putchar commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
+
 /**
- * main - Entry point
+ * main - Prints Holberton as a message.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char *sh = "Holberton";
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int count, sz;
 
-	while (*sh)
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
 	{
-		_putchar(*sh);
-		sh++;
+		_putchar(str[count]);
 	}
 	_putchar('\n');
-
 	return (0);
 }
