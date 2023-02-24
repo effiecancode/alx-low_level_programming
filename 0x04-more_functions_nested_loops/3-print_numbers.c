85e1c4215d4dd93ccef4a25a218c1bdc6e2249c1
effiecancode <faitheffie25@gmail.com>
2023-02-23 17:09:22 -0800

    The print numbers commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers for 1 to 9
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		++i;
+	}
+	_putchar('\n');
+}
+
