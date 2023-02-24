05b66bbcb84843175c560eecb0ad3bb3d26d5f8c
effiecancode <faitheffie25@gmail.com>
2023-02-23 17:11:53 -0800

    The most numbers commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints all number from 0 to 9 except 2 and 4
+ * Return: 0 on success
+ */
+
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		if (a != 2 && a != 4)
+			_putchar(a + '0');
+		++a;
+	}
+	_putchar('\n');
+}
