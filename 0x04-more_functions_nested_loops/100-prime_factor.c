ee8538f7c6ec3b029bd1d2719aef5ab0601a1d9f
effiecancode <faitheffie25@gmail.com>
2023-02-23 17:29:20 -0800

    The prime factor commit

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the largest prime factor
+ * Return: 0
+ */
+
+int main(void)
+{
+	long i;
+	long num = 612852475143;
+
+	for (i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+			num /= i;
+	}
+	printf("%li\n", num);
+	return (0);
+}
+
