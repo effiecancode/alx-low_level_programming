3553e753a9fbf53a38ab5cc8cbe3bbd8131b7ec8
effiecancode <faitheffie25@gmail.com>
2023-03-16 16:37:34 -0700

    The malloc checked commit

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+More Malloc_free
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
+int main(int argc, char *argv[]);
+
+#endif
