711aaf04c919c7e3124c0ebe03b515b40b9280f0
effiecancode <faitheffie25@gmail.com>
2023-04-03 19:21:40 -0700

    bug fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,22 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the value of elements in a linked lists
- * @h: head of  linked list named listint_t
+ * print_listint - prints all the values of the linked list
+ * @h: head of linked list of named listint_t
  *
  * Return: number of nodes
  */
-size_t listint_len(const listint_t *h)
+
+size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
 	while (h)
 	{
+		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
 
 	return (count);
 }
-
