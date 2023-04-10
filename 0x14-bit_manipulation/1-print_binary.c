71eb79503b5d402d16c9a4bbd57e6e70ef16dfc9
effiecancode <faitheffie25@gmail.com>
2023-04-10 11:14:51 -0700

    Print_binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary -  to print the binary representation of a number
+ * @n: integer to be converted
+ */
+void print_binary(unsigned long int n)
+{
+	int i;
+
+	for (i = sizeof(unsigned long int) * 8 - 1; i >= 0; i--)
+	{
+			
+		_putchar((n >> i) & 1 ? '1' : '0');
+	}
+}
