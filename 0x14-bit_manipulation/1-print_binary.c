1effd1e810f0424568cbea5ca5f4012c94711604
effiecancode <faitheffie25@gmail.com>
2023-04-10 12:18:27 -0700

    Print Binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,9 +10,9 @@ void print_binary(unsigned long int n)
 
 	if (n >> 0)
 	{
-	if (n >> 1)
-	print_binary(n >> 1);
-	_putchar((n & 1) + '0');
+	if (n >> 2)
+	print_binary(n >> 2);
+	_putchar((n & 2) + '0');
 	}
 	else
 	{
