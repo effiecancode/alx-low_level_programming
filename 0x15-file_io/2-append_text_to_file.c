6a0697a2234fc7dc62df4cdc02629d631db74376
effiecancode <faitheffie25@gmail.com>
2023-04-14 12:30:44 -0700

    Append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,25 +8,25 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-        int op, wr, leng = 0;
+	int op, wr, length = 0;
 
-        if (filename == NULL)
-                return (-1);
+	if (filename == NULL)
+		return (-1);
 
-        if (text_content != NULL)
-        {
-                for (leng = 0; text_content[leng];)
-                        leng++;
-        }
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length];)
+			length++;
+	}
 
-        op = open(filename, O_WRONLY | O_APPEND);
-        wr = write(op, text_content, leng);
+	op = open(filename, O_WRONLY | O_APPEND);
+	wr = write(op, text_content, length);
 
 
-        if (op == -1 || wr == -1)
-                return (-1);
+	if (op == -1 || wr == -1)
+		return (-1);
 
-        close(op);
+	close(op);
 
-        return (1);
+	return (1);
 }
